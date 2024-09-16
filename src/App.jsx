2fa1b8196c2ce6142d05f763f89c58c5595c2b2a
Kong_Sisovandara <sisovandara@gmail.com>
2024-09-17 00:07:11 +0700

    Homepage and component

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Footer } from './components/Footer.jsx'
+import Header from './components/Navbar.jsx'
+import { Homepage } from './pages/Homepage.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Homepage/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
